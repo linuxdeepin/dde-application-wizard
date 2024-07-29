51524faf6f1a69f0deaeb899508919048c73dccb
Weblate (bot) <hosted@weblate.org>
2024-07-29 10:32:16 +0200

    i18n: Translations update from Hosted Weblate (#24)
    * Translated using Weblate (Polish)
    * Translated using Weblate (Portuguese (Brazil))
    * Translated using Weblate (German)
    
    Translate-URL: https://hosted.weblate.org/projects/deepin/dde-application-wizard/pl/
    Translate-URL: https://hosted.weblate.org/projects/deepin/dde-application-wizard/pt_BR/
    Translate-URL: https://hosted.weblate.org/projects/deepin/dde-application-wizard/de/
    
    ---------
    
    Co-authored-by: Kacper Karpiński <kacperocik@yahoo.com>
    Co-authored-by: Rai Lima Ferreira <rai.lima17@gmail.com>
    Co-authored-by: Keine Werbung <keinewerbung@franken-online.de>

--- translations/dde-application-wizard_de.ts
+++ translations/dde-application-wizard_de.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pt_BR">
+<TS version="2.1" language="de">
 <context>
     <name>QObject</name>
     <message>
         <location filename="../dbus/launcher1compat.cpp" line="41"/>
         <source>%1 removed successfully</source>
-        <translation>%1 removido com sucesso</translation>
+        <translation>%1 erfolgreich entfernt</translation>
     </message>
     <message>
         <location filename="../dbus/launcher1compat.cpp" line="43"/>
         <source>Failed to remove the app</source>
-        <translation>A remoção do aplicativo falhou</translation>
+        <translation>App konnte nicht entfernt werden</translation>
     </message>
 </context>
 </TS>
