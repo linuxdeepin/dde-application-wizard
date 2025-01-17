02530756fc7601457e83e987b3fac97842b68927
Weblate (bot) <hosted@weblate.org>
2025-01-17 07:12:26 +0100

    i18n: Translations update from Hosted Weblate (#29)
    * Added translation using Weblate (Tamil)
    
    * Translated using Weblate (Tamil)
    
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: deepin/Application Wizard (dde-application-wizard)
    Translate-URL: https://hosted.weblate.org/projects/deepin/dde-application-wizard/ta/
    
    * Translated using Weblate (Ukrainian)
    
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: deepin/Application Wizard (dde-application-wizard)
    Translate-URL: https://hosted.weblate.org/projects/deepin/dde-application-wizard/uk/
    
    ---------
    
    Co-authored-by: தமிழ்நேரம் <anishprabu.t@gmail.com>
    Co-authored-by: Максим Горпиніч <mgorpinic2005@gmail.com>

--- translations/dde-application-wizard_ta.ts
+++ translations/dde-application-wizard_ta.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="uk_UA">
+<TS version="2.1" language="ta">
 <context>
     <name>QObject</name>
     <message>
         <location filename="../dbus/launcher1compat.cpp" line="41"/>
         <source>%1 removed successfully</source>
-        <translation>%1 видалено успішно</translation>
+        <translation>%1 வெற்றிகரமாக அகற்றப்பட்டது</translation>
     </message>
     <message>
         <location filename="../dbus/launcher1compat.cpp" line="43"/>
         <source>Failed to remove the app</source>
-        <translation>Не вдалося вилучити програму</translation>
+        <translation>பயன்பாட்டை அகற்றுவதில் தோல்வி</translation>
     </message>
 </context>
 </TS>
