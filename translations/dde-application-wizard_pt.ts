13384f2a53c07fbdf9eb33c8c5fe600eaf088728
ssantos <ssantos@web.de>
2025-08-03 21:57:06 +0200

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: deepin/Application Wizard (dde-application-wizard)
    Translate-URL: https://hosted.weblate.org/projects/deepin/dde-application-wizard/pt/

--- translations/dde-application-wizard_pt.ts
+++ translations/dde-application-wizard_pt.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../dbus/launcher1compat.cpp" line="41"/>
         <source>%1 removed successfully</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 removido com sucesso</translation>
     </message>
     <message>
         <location filename="../dbus/launcher1compat.cpp" line="43"/>
         <source>Failed to remove the app</source>
-        <translation type="unfinished"></translation>
+        <translation>A remoção da app falhou</translation>
     </message>
 </context>
 </TS>
